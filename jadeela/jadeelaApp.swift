a068626762f1f2bfea832bf8f36fa3b6a2badf8f
ghadeer0440 <ghadeer.a.alrakaf@gmail.com>
2023-11-23 10:37:12 +0300

    Initial Commit

---
+++ jadeela/jadeelaApp.swift
@@ -0,0 +1,17 @@
+//
+//  jadeelaApp.swift
+//  jadeela
+//
+//  Created by Ghadeer on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct jadeelaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
